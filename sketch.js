88be6a78451ad68e7372150c43e2f8a9b15e6b6d
syberant <sybrandaarnoutse@MacBook-Air-van-Sybrand.local>
2017-03-21 15:28:13 +0100

    update

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
 var cells = []
 const cellwidth = 10
+var rounds = 0
+var maxAlive = 0
 
 function setup () {
   createCanvas(windowWidth, windowHeight)
   frameRate(10)
+  colorMode(HSB, 100)
   // init cells
   for (let x = 0; x < width / cellwidth; x++) {
     var row = []
@@ -16,28 +19,42 @@ function setup () {
   console.clear()
   console.log('Hi there!🙌')
   console.log('Welcome to this implementation of Conway\'s Game of Life.')
-  console.log('This is created by splintah: https://www.github.com/splintah')
+  console.log('This is based on code by splintah: https://www.github.com/splintah')
 
 }
 
 function draw () {
-  background(0)
+    background(0)
+    
+    // drawing properties
+    fill(255)
+    noStroke()
+    
+    if (rounds > 1000) {
+        endScreen()
+        console.log("Game is finshed, round "+rounds);
+    }
+    else {
+        display()
+        calculateNeighbours()
+        update()
+    }
+}
 
-  // drawing properties
-  fill(255)
-  noStroke()
-
+function display() {
   // display
   for (let x = 1; x < width / cellwidth - 1; x++) {
     for (let y = 1; y < height / cellwidth - 1; y++) {
       cells[x][y].display()
-      // reset neighbours
       cells[x][y].neighbours = 0
     }
   }
+}
 
+function calculateNeighbours() {
   for (let x = 1; x < (width / cellwidth) - 1; x++) {
     for (let y = 1; y < (height / cellwidth) - 1; y++) {
+      //cells[x][y].neigbours = 0
       for (let i = -1; i <= 1; i++) {
         for (let j = -1; j <= 1; j++) {
           if (x + i > 0 && x + i <= width / cellwidth && y + j > 0 && y + j <= height / cellwidth) {
@@ -53,8 +70,9 @@ function draw () {
       }
     }
   }
+}
 
-
+function update() {
   // update cells
   for (let x = 0; x < (width / cellwidth); x++) {
     for (let y = 0; y < (height / cellwidth); y++) {
@@ -69,13 +87,27 @@ function draw () {
       }
     }
   }
+  
+  rounds++
 }
+
+function endScreen() {
+    for (let x = 0; x < (width / cellwidth); x++) {
+        for (let y = 0; y < (height / cellwidth); y++) {
+            cells[x][y].calculateColor()
+        }
+    }
+}
+
+
 
 function Cell (x, y, cellwidth) {
   this.x = x * cellwidth
   this.y = y * cellwidth
   this.neighbours = 0
   this.alive = true
+  this.deadCounter = 0
+  this.aliveCounter = 0
 
   var chance = round(random(0, 2))
   if (chance === 1) {
@@ -92,9 +124,18 @@ function Cell (x, y, cellwidth) {
 
   this.kill = function () {
     this.alive = false
+    this.deadCounter++
   }
 
   this.live = function () {
     this.alive = true
+    this.aliveCounter++
+    if (this.aliveCounter > maxAlive) {maxAlive = this.aliveCounter}
   }
+  
+  this.calculateColor = function () {
+    var percentageAlive = this.aliveCounter / ((this.deadCounter + this.aliveCounter) - maxAlive) * 100 
+    fill(percentageAlive, 100, 100)
+    rect(this.x, this.y, cellwidth, cellwidth)
+    }
 }
